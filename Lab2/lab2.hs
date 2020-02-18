11eac8fe35c9ea85d2f1a07b76cff957cb041ce6
Shahin <47277897+shashabinks@users.noreply.github.com>
2020-02-18 11:33:08 +0000

    done_q2

--- Lab2/lab2.hs
+++ Lab2/lab2.hs
@@ -5,4 +5,12 @@ data Tree a = Br a (Tree a) (Tree a) | Lf
 linear :: Int -> Tree Int
 linear 0 = Lf
 linear 1 = (Br 1 Lf Lf)
-linear n = (Br n  (linear(n-2)) (linear (n-1)))
+linear n = (Br n Lf (linear (n-1)))
+
+inorder :: Tree a -> [a]
+inorder Lf = []
+inorder (Br n ls rs) = inorder ls ++ [n] ++ inorder rs 
+
+preorder :: Tree a -> [a]
+preorder Lf = []
+preorder (Br n ls rs) = preorder ls ++ preorder rs ++ [n]
